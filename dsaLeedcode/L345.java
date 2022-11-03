c9df2b31f7ebf65c331c604ef97fcf7399f966be
kumkum <kumkumrathour00@gmail.com>
2022-11-03 15:41:51 +0530

    add This

---
+++ dsaLeedcode/L345.java
@@ -0,0 +1,23 @@
+// class Solution {
+//     public String reverseVowels(String s) {
+//         if(s == null || s.length() == 0) return s;
+//         String vowels = "aeiouAEIOU";
+//         char[] chars = s.toCharArray();
+//         int i = 0 ;
+//         int j = s.length()-1;
+//         while(i<j){
+//                 while(i<j && !vowels.contains(chars[i]+"")){
+//                     i++;
+//                 }
+//                 while(i<j && !vowels.contains(chars[j]+"")){
+//                     j--;
+//                 }
+//                 char  temp = chars[i];
+//                 chars[i] =chars[j];
+//                 chars[j] = temp;
+//                 i++;
+//                 j--;
+//             }
+//             return new String(chars);
+//         }
+//     }
