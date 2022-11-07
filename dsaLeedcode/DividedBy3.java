90787b4c3ae25f71a981f42dfacf7819a2d541a5
kumkum <kumkumrathour00@gmail.com>
2022-11-07 07:44:00 +0530

    DividedBy3

---
+++ dsaLeedcode/DividedBy3.java
@@ -0,0 +1,17 @@
+package dsaleedcode;
+
+public class DividedBy3 {
+    class Solution {
+        public int averageValue(int[] nums) {
+            int count = 0;
+            int sum = 0 ;
+             for( int num :nums)  {
+               if(num%6 == 0){
+                   sum += num ;
+                    count++;
+               }
+                }
+               return count ==0 ? 0 : Math.round(sum/count);  
+        } 
+    }
+}
